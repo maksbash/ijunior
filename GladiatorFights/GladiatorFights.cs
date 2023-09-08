78066099628e0c7f9aa96322000f30e95ba8a7fa
Maxim Basharov <maksbash@gmail.com>
2023-09-08 08:02:23 +0300

    start gladiators fights

---
+++ GladiatorFights/GladiatorFights.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Gladiator
+{
+    private string _name;
+    private int _health;
+    private int _damage;
+    private int _armor; 
+
+    public void TakeDamage(int damage)
+    {
+        _health -= damage - _armor;
+    }
+
+}
