5d19fe9a62ca326e3ede73f0cd77b583292ab791
Maxim Basharov <maksbash@gmail.com>
2023-10-15 22:10:06 +0300

    Start CarService

---
+++ CarService/CarService.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class PartsWarehouse
+{
+    private List<Part> _parts;
+
+    public PartsWarehouse()
+    {
+        _parts = new List<Part>();
+        AddParts();
+    }
+
+    private void AddParts()
+    {
+
+    }
+}
+
+class Part
+{
+    public Part(string type, int price)
+    {
+        Type = type;
+        Price = price;
+    }
+
+    public string Type { get; private set; }
+    public int Price { get; private set; }
+}
