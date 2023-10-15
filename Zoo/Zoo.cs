750dc2a53ab72459716eaf81ffdb3bc7ac87a116
Maxim Basharov <maksbash@gmail.com>
2023-10-15 21:40:49 +0300

    New Zoo

--- Zoo/Zoo.cs
+++ Zoo/Zoo.cs
@@ -1,8 +1,87 @@
-﻿internal class Zoo
+﻿using System.CodeDom.Compiler;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Zoo zoo = new Zoo();
+        zoo.ShowMenu();
+    }
+}
+
+class Zoo
+{
+    private List<Aviary> _aviaries = new List<Aviary>();
+
+    public Zoo() 
+    {
+        GenerateAviaries();
+    }
+
+    public int AviariesCount 
+    { 
+        get 
+        { 
+            return _aviaries.Count; 
+        }
+
+        private set { }
+    }
+
+    public void ShowMenu()
+    {
+        const string CommandToExit = "exit";
+
+        bool isActive = true;
+        int userNumber = -1;
+
+        while (isActive)
+        {
+            Console.Clear();
+            Console.WriteLine($"В зоопарке находится {AviariesCount} вольеров");
+            Console.Write($"Введите номер вальера чтобы подойти или {CommandToExit} для выхода: ");
+            string userCommand = Console.ReadLine();
+            if (userCommand == CommandToExit)
+            {
+                isActive = false;
+            }
+            else
+            {
+
+                int.TryParse(userCommand, out userNumber);
+
+                if (userNumber < 1 || userNumber > AviariesCount)
+                {
+                    Console.WriteLine("Вы ввели неверное число, попробуйте ещё раз.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    _aviaries[--userNumber].ShowInfo();
+                }
+            }
+        }
+    }
+
+    private void GenerateAviaries()
+    {
+        int minAnimals = 2;
+        int maxAnimals = 200;
+
+        int giraffeCount = UserUtils.GenerateRandomNumber(minAnimals, maxAnimals);
+        int zebraCount = UserUtils.GenerateRandomNumber(minAnimals, maxAnimals);
+        int pantherCount = UserUtils.GenerateRandomNumber(minAnimals, maxAnimals);
+        int lionCount = UserUtils.GenerateRandomNumber(minAnimals, maxAnimals);
+
+        Aviary giraffeAviary = new Aviary(new Giraffe(Gender.Male), giraffeCount);
+        Aviary zebraAviary = new Aviary(new Zebra(Gender.Male), zebraCount);
+        Aviary pantherAviary = new Aviary(new Panther(Gender.Male), pantherCount);
+        Aviary lionAviary = new Aviary(new Lion(Gender.Male), lionCount);
+
+        _aviaries.Add(giraffeAviary);
+        _aviaries.Add(zebraAviary);
+        _aviaries.Add(pantherAviary);
+        _aviaries.Add(lionAviary);
     }
 }
 
@@ -15,9 +94,7 @@ class Aviary
         _animals = new List<Animal>();
 
         for (int i = 0; i < count; i++) 
-        { 
-            _animals.Add(animal);
-        }
+            _animals.Add(animal.Clone());
     }
 
     public int Count
@@ -27,6 +104,14 @@ class Aviary
             return _animals.Count;
         }
     }
+
+    public void ShowInfo()
+    {
+        Console.Clear();
+        Console.WriteLine($"В вольере нажодится животное {_animals[0].Name}");
+        Console.WriteLine($"Насчитывается {Count} осыбей");
+        Console.ReadKey();
+    }
 }
 
 class Giraffe : Animal
@@ -35,6 +120,12 @@ class Giraffe : Animal
     {
         Gender = gender;
         Sound = "Уиххаа";
+        Name = "Жираф";
+    }
+
+    public override Giraffe Clone()
+    {
+        return new Giraffe(UserUtils.GetRandomGender());
     }
 }
 
@@ -44,6 +135,12 @@ class Zebra : Animal
     {
         Gender = gender;
         Sound = "Тыгыдык";
+        Name = "Зебра";
+    }
+
+    public override Zebra Clone()
+    {
+        return new Zebra(UserUtils.GetRandomGender());
     }
 }
 
@@ -53,6 +150,12 @@ class Panther : Animal
     {
         Gender = gender;
         Sound = "Ауррр";
+        Name = "Пантера";
+    }
+
+    public override Panther Clone()
+    {
+        return new Panther(UserUtils.GetRandomGender());
     }
 }
 
@@ -62,6 +165,12 @@ class Lion : Animal
     {
         Gender = gender;
         Sound = "Рррр";
+        Name = "Лев";
+    }
+
+    public override Lion Clone()
+    {
+        return new Lion(UserUtils.GetRandomGender());
     }
 }
 
@@ -69,10 +178,32 @@ abstract class Animal
 {
     public Gender Gender { get; protected set; }
     public string Sound { get; protected set; }
+    public string Name { get; protected set; }
+
+    public abstract Animal Clone();
 }
 
 enum Gender
 {
     Male,
     Female
+}
+
+class UserUtils
+{
+    private static Random _random = new Random();
+
+    public static Gender GetRandomGender()
+    {
+        int minGender = 0;
+        int maxGender = 1;
+        int genderIndex = _random.Next(minGender, maxGender + 1);
+
+        return (Gender)genderIndex;
+    }
+
+    public static int GenerateRandomNumber(int min, int max)
+    {
+        return _random.Next(min, max);
+    }
 }
