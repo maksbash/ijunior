5dec9211d0be132cf5332254b44e6ec9220aa4e7
Basharov Maxim <maksbash@gmail.com>
2023-11-22 14:20:08 +0300

    UnionTroops

---
+++ UnionTroops/UnionTroops.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Solder> troopFirst = new List<Solder>
+        {
+            new Solder("Иванов Иван Иванович", "Автомат", "Солдат", 55),
+            new Solder("Бирюков Максим Максимович", "Пистолет", "Полковник", 440),
+            new Solder("Петров Петр Петрович", "Пулемет", "Генерал", 321),
+            new Solder("Бодров Федор Григорьевич", "Граната", "Подполковник", 61),
+            new Solder("Егорова Анастасия Максимовна", "Нож", "Майор", 788),
+        };
+
+        List<Solder> troopSecond = new List<Solder>
+        {
+            new Solder("Шапкин Алексей Дмитриевич", "Гранатомет", "Полковник", 129),
+            new Solder("Чувак Ядрён Болеющий", "Нож", "Солдат", 33),
+            new Solder("Круглова Алёна Антоновна", "Автомат", "Генерал", 200),
+            new Solder("Плющ Артём Колбасович", "Пистолет", "Солдат", 24),
+            new Solder("Герасимов Герась Герасьевич", "Автомат", "Солдат", 99),
+        };
+
+        char latter = 'Б';
+
+        var unionedTroop = troopSecond.Union(troopFirst.Where(troopFirst => troopFirst.Name.ToUpper().StartsWith(latter)));
+
+        foreach (var solder in unionedTroop)
+            Console.WriteLine($"{solder.Name} - {solder.Rank}");
+
+        Console.ReadKey();
+    }
+}
+
+class Solder
+{
+    public Solder(string name, string weapons, string rank, int lifeTime)
+    {
+        Name = name;
+        Weapons = weapons;
+        Rank = rank;
+        LifeTime = lifeTime;
+
+    }
+
+    public string Name { get; private set; }
+    public string Weapons { get; private set; }
+    public string Rank { get; private set; }
+    public int LifeTime { get; private set; }
+}
